d54b83c94fe94ed33f0441babafe5ab720edf7bc
SEM <SEM@DESKTOP-4GKM6HK>
2022-09-23 20:24:00 +0300

    Add project files.

---
+++ KrediKart/Customer.cs
@@ -0,0 +1,51 @@
+﻿namespace Kredi
+
+{
+    public class Customer    
+    {
+        public string kredikartno;
+        public string _kredikartno
+        {
+            get
+            {
+                return "************" + kredikartno.Substring(12);
+            }
+
+            set
+            {
+                bool kontrol = false;
+                if (value.Length == 16)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool sayiMi = char.IsNumber(value[i]);//12345678
+                        if (sayiMi)
+                        {
+                            //veri geçerlidir
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    if (kontrol)
+                    {
+                        Console.WriteLine("Geçerli bir bilgi giriniz.");
+                    }
+                    else
+                    {
+                        kredikartno = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hane sayısı yanlış.");
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ KrediKart/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Kredi
+{
+    internal class Program //normal değişkenlerde field kullanırken private değişkenlerde prop kullanıyoruz
+
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer._kredikartno = "1234567891234567";
+            Console.WriteLine("Girdiğiniz Değerler" + customer._kredikartno);
+        }
+    }
+}
